c95a9d0156cb0a2428dcda1d59d4451ddc3859b7
msk <mohammadsadeghmk@gmail.com>
2020-06-22 20:03:09 +0430

    PageDetails Component + Test

---
+++ src/components/PageDetails/PageDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import classes from './OrangeButton.module.scss';
+
+const OrangeButton = props => {
+    return (
+        <button className={classes.OrangeButton} onClick={props.onClick}>
+            {props.text}
+        </button>
+    );
+};
+
+export default OrangeButton;
+
+OrangeButton.propTypes = {
+    text: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node,
+        PropTypes.arrayOf(PropTypes.node)
+    ]),
+    onClick: PropTypes.func
+};
